a16868d4c0b0c93466a274b00d94a104eca9356a
oon06021 <oon0602107@gmail.com>
2020-01-15 11:06:51 +0900

    original

---
+++ app/src/main/java/com/example/ssokk11/infomation.kt
@@ -0,0 +1,14 @@
+package com.example.ssokk11
+
+//import android.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class infomation : AppCompatActivity() {
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_infomation)
+    }
+}
