737b4dd4c385cd51b945170e5e75e1c98fcfca5d
YeRamJung <jyr2356@gmail.com>
2020-01-15 14:45:26 +0900

    made MyExercise

---
+++ app/src/main/java/com/example/ssokk11/WeeklyBloodSugar.kt
@@ -0,0 +1,12 @@
+package com.example.ssokk11
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class WeeklyBloodSugar : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_weekly_blood_sugar)
+    }
+}
